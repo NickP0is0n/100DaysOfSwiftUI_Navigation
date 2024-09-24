5823a010b072c8678d3f62edb87405e28935478f
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-24 19:12:29 +0300

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Mykola Chaikovskyi on 24.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
