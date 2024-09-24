7a46bfad0d405d05fd6ce03b3b16c0ca59b9d9d8
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-24 23:51:38 +0300

    Hashable demo

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,24 +7,21 @@
 
 import SwiftUI
 
-struct DetailView: View {
-    var number: Int
-    
-    var body: some View {
-        Text("Detail View \(number)")
-    }
-    
-    init(number: Int) {
-        self.number = number
-        print("Creating detail view \(number).")
-    }
+struct Student: Hashable {
+    var id = UUID()
+    var name: String
+    var age: Int
 }
 
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            NavigationLink("Tap me") {
-                DetailView(number: 666)
+            List(0..<100) { i in
+                NavigationLink("Select \(i)", value: i)
+            } .navigationDestination(for: Int.self) { selection in
+                Text("You selected \(selection)")
+            } .navigationDestination(for: Student.self) { destination in
+                Text("This is \(destination.name) (\(destination.age) y.o.)")
             }
         }
     }
