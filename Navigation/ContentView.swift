f43ba0b1bfdd1734aab838a4b44ca99c58610d67
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-25 23:41:28 +0300

    NavigationPath demo

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var path = NavigationPath()
+    
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $path) {
             List {
                 ForEach(0..<5) { i in
                     NavigationLink("Select number: \(i)", value: i)
@@ -19,11 +21,20 @@ struct ContentView: View {
                     NavigationLink("Select string: \(i)", value: String(i))
                 }
             }
+            .toolbar {
+                Button("Push 556") {
+                    path.append(556)
+                }
+                
+                Button("Push Hello") {
+                    path.append("Hello")
+                }
+            }
             .navigationDestination(for: Int.self) { selection in
                  Text("You have selected the \(selection) number")
             }
             .navigationDestination(for: String.self) { selection in
-                Text("You have selected the \(selection) string") 
+                Text("You have selected the \(selection) string")
             }
         }
     }
