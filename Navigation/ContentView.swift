daf7c2789b2ea1c211a75577604530717d15f5c1
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-27 00:40:53 +0300

    Editable navigation bar demo

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -8,23 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = "SwiftUI"
+    
     var body: some View {
         NavigationStack {
             Text("Hello!")
-                .toolbar {
-                    ToolbarItem(placement: .confirmationAction) {
-                        Button("Tap me!") {
-                            // button action code
-                        }
-                    }
-                    
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button("Tap me 2!") {
-                            // button action code
-                        }
-                    }
-                }
-                .navigationBarBackButtonHidden()
+                .navigationTitle($name)
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
