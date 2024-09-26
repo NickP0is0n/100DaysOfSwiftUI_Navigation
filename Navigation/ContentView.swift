5a82218961264042ae74c0c3d11776dc0a3f150d
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-27 00:30:34 +0300

    ToolbarItem demo

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,62 +7,24 @@
 
 import SwiftUI
 
-class PathStore: ObservableObject {
-    @Published var path: NavigationPath {
-        didSet {
-            save()
-        }
-    }
-    
-    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
-    
-    init() {
-        if let data = try? Data(contentsOf: savePath) {
-            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation .self, from: data) {
-                path = NavigationPath(decoded)
-                return
-            }
-        }
-        
-        path = NavigationPath()
-    }
-    
-    func save() {
-        guard let representation = path.codable else { return }
-        
-        do {
-            let data = try JSONEncoder().encode(representation )
-            try data.write(to: savePath)
-        } catch {
-            print("Failed to save navigation data.")
-        }
-    }
-}
-
-struct DetailView: View {
-    var number: Int
-    @Binding var path: NavigationPath
-    
-    var body: some View {
-        NavigationLink("Go to the random number", value: Int.random(in: 1...1000))
-            .navigationTitle("Number: \(number)")
-            .toolbar {
-                Button("Home") {
-                    path = NavigationPath()
-                }
-            }
-    }
-}
-
 struct ContentView: View {
-    @State private var pathStore = PathStore()
-    
     var body: some View {
-        NavigationStack(path: $pathStore.path) {
-            DetailView(number: 0, path: $pathStore.path)
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $pathStore.path )
+        NavigationStack {
+            Text("Hello!")
+                .toolbar {
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button("Tap me!") {
+                            // button action code
+                        }
+                    }
+                    
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("Tap me 2!") {
+                            // button action code
+                        }
+                    }
                 }
+                .navigationBarBackButtonHidden()
         }
     }
 }
